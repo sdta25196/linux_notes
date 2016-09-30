ae3e214d82ca62c38db7c271608fff70c7b1808e
hienha <hienha@163.com>
2016-10-01 02:11:54 +0800

    test iterator in dic with for statement

---
+++ Python/codes/1.8_dict.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+dic = dict(a = 1, b = 2, c = 3)
+
+print "The generate dict is: %s" % (dic)
+
+for k in dic:
+	print "The key is %s, and the value is %s" % (k, dic[k])

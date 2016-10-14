230c4955e6eb0001c7f15018bfb4748bda06875f
hienha <hienha@163.com>
2016-10-14 13:20:09 +0800

    add output messages

--- Linux/5-Shell编程/1_Shell编程进阶/1.4_variables.sh
+++ Linux/5-Shell编程/1_Shell编程进阶/1.4_variables.sh
@@ -1,4 +1,24 @@
-#!/b/bash
+#!/bin/bash
+# Date: 2016-10-14 13:18
+# Purpose: Test the bash builtin variables
+# Output: 
+##########################################################
+# sh 1.4_variables.sh 1 a 2 b 3 c 4 d "hello'\n'"
+# echo $0... ...
+# 1.4_variables.sh
+# echo $1... ...
+# 1
+# echo $2... ..
+# a
+# echo $3... ...
+# 2
+# echo $*... ...
+# 1 a 2 b 3 c 4 d hello'\n'
+# echo $#... ...
+# 9
+# echo $@... ...
+# 1 a 2 b 3 c 4 d hello'\n'
+##########################################################
 
 echo 'echo $0... ...'
 echo $0
@@ -6,9 +26,8 @@ echo $0
 echo 'echo $1... ...'
 echo $1
 
-echo 'echo $2\... ..'
+echo 'echo $2... ..'
 echo $2
-
 
 echo 'echo $3... ...'
 echo $3
@@ -18,3 +37,6 @@ echo $*
 
 echo 'echo $#... ...'
 echo $#
+
+echo 'echo $@... ...'
+echo $@

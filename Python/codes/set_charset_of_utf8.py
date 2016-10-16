2385e594ed71b69886841aba9c91f6eb4acd8dfd
hienha <hienha@163.com>
2016-10-16 21:52:17 +0800

    add two files.

---
+++ Python/codes/set_charset_of_utf8.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#encoding:utf8
+#encoding:utf-8
+#coding:utf8
+#coding:utf-8
+# -*- coding:utf8 -*- 
+# -*- coding:utf-8 -*- 
+# -*- encoding:utf8 -*- 
+# -*- encoding:utf-8 -*- 
+
+print '中文测试'

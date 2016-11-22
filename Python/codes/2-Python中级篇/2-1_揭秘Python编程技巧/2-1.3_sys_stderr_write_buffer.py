0a7d224e6af10967affa37c822ba3850228d59a3
zhangwen <zhangwen@ichunqiu.com>
2016-11-22 19:18:11 +0800

    sys.stdout.write('\n') ==> print

--- Python/codes/2-Python中级篇/2-1_揭秘Python编程技巧/2-1.3_sys_stderr_write_buffer.py
+++ Python/codes/2-Python中级篇/2-1_揭秘Python编程技巧/2-1.3_sys_stderr_write_buffer.py
@@ -6,5 +6,5 @@ import sys, time
 for i in xrange(10):
     sys.stdout.write("str:%d " % i)
     time.sleep(1)
-    #sys.stdout.write('\n')
+    #sys.stdout.write('\n') """这是因为sys.stdout.write('\n')就相当于调用了print，所以就不会缓存了"""
     sys.stdout.flush()
